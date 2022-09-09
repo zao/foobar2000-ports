d25b4b8925e877c889d71d4601bdd06db0c4bd2a
Lars Viklund <zao@zao.se>
2022-09-09 22:56:09 +0200

    Add columns UI SDK 7.0.0-beta1

---
+++ ports/columns-ui-sdk/portfile.cmake
@@ -0,0 +1,18 @@
+vcpkg_from_git(
+    OUT_SOURCE_PATH SOURCE_PATH
+    URL https://github.com/reupen/columns_ui-sdk
+    REF d8ca0a1fe727f1416be25cdb2b059b62228a7e2e
+)
+
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+)
+
+vcpkg_install_cmake()
+vcpkg_copy_pdbs()
+
+vcpkg_cmake_config_fixup(PACKAGE_NAME unofficial-${PORT} CONFIG_PATH share/unofficial-${PORT})
+
+configure_file(${SOURCE_PATH}/sdk-license.txt ${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright COPYONLY)
