f473f1c47653793cfb42856e9f5c1525023a380c
Lars Viklund <zao@zao.se>
2022-11-03 21:33:54 +0100

    Update columns-ui-sdk to 7.0.0-beta2

--- ports/columns-ui-sdk/portfile.cmake
+++ ports/columns-ui-sdk/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL https://github.com/reupen/columns_ui-sdk
-    REF d8ca0a1fe727f1416be25cdb2b059b62228a7e2e
+    REF f57c12948ee28a8b108194574f1d08d9fe4a2237
 )
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
